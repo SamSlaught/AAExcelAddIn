ce9dbadbe9150848345fc56762db6899bc453b82
Sam Slaughterbeck <samslaught4him@outlook.com>
2016-11-08 17:29:00 -0500

    created navigator form (#2)

---
+++ AAExcelAddIn/AAExcelAddIn/ribMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace AAExcelAddIn
+{
+    public partial class ribMain
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+        }
+
+        //User clicks to open the navigator form
+        private void btnNavigator_Click(object sender, RibbonControlEventArgs e)
+        {
+            PvtLstObjNavigator navigator = new PvtLstObjNavigator();
+            navigator.Show();
+        }
+    }
+}
