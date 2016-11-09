7c4a3c7ba98c81c355c7581534373d30799b8fce
Sam Slaughterbeck <samslaught4him@outlook.com>
2016-11-09 12:37:32 -0500

    added disable ribbon functionality for no workbooks opened scenario (#3)

--- AAExcelAddIn/AAExcelAddIn/ThisAddIn.cs
+++ AAExcelAddIn/AAExcelAddIn/ThisAddIn.cs
@@ -12,12 +12,53 @@ namespace AAExcelAddIn
 {
     public partial class ThisAddIn
     {
+
+        //Globals
+        public ribMain ribbon;
+
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+
+            //Storing the ribbon object globally
+            Type type = typeof(ribMain);
+            ribbon = Globals.Ribbons.GetRibbon(type) as ribMain;
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
+        }
+
+        //Excel Events
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private void Excel_WorkbookBeforeClose(Excel.Workbook Wb, ref bool Cancel)
+        {
+            if (Wb.Application.Workbooks.Count == 1)
+            {
+                ToggleRibbonLock(true);
+            }
+        }
+
+        private void Excel_NewWorkbook(Excel.Workbook Wb)
+        {
+            if (Wb.Application.Workbooks.Count == 1)
+            {
+                ToggleRibbonLock(false);
+            }
+        }
+
+        private void Excel_WorkbookOpen(Excel.Workbook Wb)
+        {
+            if (Wb.Application.Workbooks.Count == 1)
+            {
+                ToggleRibbonLock(false);
+            }
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        //Toggles certain controls to be enable/disabled for certain conditions with the application
+        void ToggleRibbonLock(bool lockRibbon)
+        {
+            ribbon.btnNavigator.Enabled = !lockRibbon;
         }
 
         #region VSTO generated code
@@ -30,6 +71,11 @@ namespace AAExcelAddIn
         {
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
             this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+
+            //Excel Events
+            ((Excel.AppEvents_Event)this.Application).NewWorkbook += new Microsoft.Office.Interop.Excel.AppEvents_NewWorkbookEventHandler(Excel_NewWorkbook);
+            this.Application.WorkbookOpen += new Excel.AppEvents_WorkbookOpenEventHandler(Excel_WorkbookOpen);
+            this.Application.WorkbookBeforeClose += new Excel.AppEvents_WorkbookBeforeCloseEventHandler(Excel_WorkbookBeforeClose);
         }
         
         #endregion
