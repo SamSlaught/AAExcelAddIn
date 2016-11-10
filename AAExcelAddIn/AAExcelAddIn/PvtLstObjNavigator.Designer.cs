410ca824fafc10b94c7cb98088d9d61ef9de7b2f
Sam Slaughterbeck <sam@streamlinedstudio.com>
2016-11-10 10:33:47 -0500

    double click functionality and datagrid fields columns

--- AAExcelAddIn/AAExcelAddIn/PvtLstObjNavigator.Designer.cs
+++ AAExcelAddIn/AAExcelAddIn/PvtLstObjNavigator.Designer.cs
@@ -34,13 +34,20 @@
             this.pgeListObjects = new System.Windows.Forms.TabPage();
             this.dgrListObjects = new System.Windows.Forms.DataGridView();
             this.PivotTable = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PvtWorksheet = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.PvtDataSourceName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.PvtDataSourceType = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.PvtDataSourceDesc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PvtPageFields = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PvtColumnFields = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PvtRowFields = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PvtDataFields = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Table = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LstObjWorksheet = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LstObjDataSource = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LstObjDataSourceType = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LstObjDataSourceDesc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LstObjColumns = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tcrNavigator.SuspendLayout();
             this.pgePivotTables.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgrPivotTables)).BeginInit();
@@ -56,7 +63,7 @@
             this.tcrNavigator.Name = "tcrNavigator";
             this.tcrNavigator.SelectedIndex = 0;
             this.tcrNavigator.Size = new System.Drawing.Size(638, 370);
-            this.tcrNavigator.TabIndex = 1;
+            this.tcrNavigator.TabIndex = 0;
             // 
             // pgePivotTables
             // 
@@ -73,18 +80,27 @@
             // 
             this.dgrPivotTables.AllowUserToAddRows = false;
             this.dgrPivotTables.AllowUserToDeleteRows = false;
+            this.dgrPivotTables.AllowUserToResizeRows = false;
+            this.dgrPivotTables.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             this.dgrPivotTables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgrPivotTables.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.PivotTable,
+            this.PvtWorksheet,
             this.PvtDataSourceName,
             this.PvtDataSourceType,
-            this.PvtDataSourceDesc});
+            this.PvtDataSourceDesc,
+            this.PvtPageFields,
+            this.PvtColumnFields,
+            this.PvtRowFields,
+            this.PvtDataFields});
             this.dgrPivotTables.Location = new System.Drawing.Point(6, 6);
             this.dgrPivotTables.Name = "dgrPivotTables";
             this.dgrPivotTables.ReadOnly = true;
             this.dgrPivotTables.RowHeadersVisible = false;
+            this.dgrPivotTables.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgrPivotTables.Size = new System.Drawing.Size(618, 335);
             this.dgrPivotTables.TabIndex = 1;
+            this.dgrPivotTables.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgrPivotTables_CellMouseDoubleClick);
             // 
             // pgeListObjects
             // 
@@ -101,24 +117,36 @@
             // 
             this.dgrListObjects.AllowUserToAddRows = false;
             this.dgrListObjects.AllowUserToDeleteRows = false;
+            this.dgrListObjects.AllowUserToResizeRows = false;
+            this.dgrListObjects.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             this.dgrListObjects.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgrListObjects.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Table,
+            this.LstObjWorksheet,
             this.LstObjDataSource,
             this.LstObjDataSourceType,
-            this.LstObjDataSourceDesc});
+            this.LstObjDataSourceDesc,
+            this.LstObjColumns});
             this.dgrListObjects.Location = new System.Drawing.Point(7, 7);
             this.dgrListObjects.Name = "dgrListObjects";
             this.dgrListObjects.ReadOnly = true;
             this.dgrListObjects.RowHeadersVisible = false;
+            this.dgrListObjects.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgrListObjects.Size = new System.Drawing.Size(617, 331);
             this.dgrListObjects.TabIndex = 0;
+            this.dgrListObjects.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgrListObjects_CellDoubleClick);
             // 
             // PivotTable
             // 
             this.PivotTable.HeaderText = "PivotTable";
             this.PivotTable.Name = "PivotTable";
             this.PivotTable.ReadOnly = true;
+            // 
+            // PvtWorksheet
+            // 
+            this.PvtWorksheet.HeaderText = "Worksheet";
+            this.PvtWorksheet.Name = "PvtWorksheet";
+            this.PvtWorksheet.ReadOnly = true;
             // 
             // PvtDataSourceName
             // 
@@ -141,11 +169,45 @@
             this.PvtDataSourceDesc.ReadOnly = true;
             this.PvtDataSourceDesc.Width = 300;
             // 
+            // PvtPageFields
+            // 
+            this.PvtPageFields.HeaderText = "Filter Fields";
+            this.PvtPageFields.Name = "PvtPageFields";
+            this.PvtPageFields.ReadOnly = true;
+            this.PvtPageFields.Width = 150;
+            // 
+            // PvtColumnFields
+            // 
+            this.PvtColumnFields.HeaderText = "Column Fields";
+            this.PvtColumnFields.Name = "PvtColumnFields";
+            this.PvtColumnFields.ReadOnly = true;
+            this.PvtColumnFields.Width = 150;
+            // 
+            // PvtRowFields
+            // 
+            this.PvtRowFields.HeaderText = "Row Fields";
+            this.PvtRowFields.Name = "PvtRowFields";
+            this.PvtRowFields.ReadOnly = true;
+            this.PvtRowFields.Width = 150;
+            // 
+            // PvtDataFields
+            // 
+            this.PvtDataFields.HeaderText = "Value Fields";
+            this.PvtDataFields.Name = "PvtDataFields";
+            this.PvtDataFields.ReadOnly = true;
+            this.PvtDataFields.Width = 150;
+            // 
             // Table
             // 
             this.Table.HeaderText = "Table";
             this.Table.Name = "Table";
             this.Table.ReadOnly = true;
+            // 
+            // LstObjWorksheet
+            // 
+            this.LstObjWorksheet.HeaderText = "Worksheet";
+            this.LstObjWorksheet.Name = "LstObjWorksheet";
+            this.LstObjWorksheet.ReadOnly = true;
             // 
             // LstObjDataSource
             // 
@@ -168,12 +230,22 @@
             this.LstObjDataSourceDesc.ReadOnly = true;
             this.LstObjDataSourceDesc.Width = 300;
             // 
+            // LstObjColumns
+            // 
+            this.LstObjColumns.HeaderText = "Columns";
+            this.LstObjColumns.Name = "LstObjColumns";
+            this.LstObjColumns.ReadOnly = true;
+            this.LstObjColumns.Width = 150;
+            // 
             // PvtLstObjNavigator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(662, 394);
             this.Controls.Add(this.tcrNavigator);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "PvtLstObjNavigator";
             this.Text = "PivotTable/List Object Navigator";
             this.Load += new System.EventHandler(this.PvtLstObjNavigator_Load);
@@ -194,12 +266,19 @@
         private System.Windows.Forms.TabPage pgeListObjects;
         private System.Windows.Forms.DataGridView dgrListObjects;
         private System.Windows.Forms.DataGridViewTextBoxColumn PivotTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PvtWorksheet;
         private System.Windows.Forms.DataGridViewTextBoxColumn PvtDataSourceName;
         private System.Windows.Forms.DataGridViewTextBoxColumn PvtDataSourceType;
         private System.Windows.Forms.DataGridViewTextBoxColumn PvtDataSourceDesc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PvtPageFields;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PvtColumnFields;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PvtRowFields;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PvtDataFields;
         private System.Windows.Forms.DataGridViewTextBoxColumn Table;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LstObjWorksheet;
         private System.Windows.Forms.DataGridViewTextBoxColumn LstObjDataSource;
         private System.Windows.Forms.DataGridViewTextBoxColumn LstObjDataSourceType;
         private System.Windows.Forms.DataGridViewTextBoxColumn LstObjDataSourceDesc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LstObjColumns;
     }
 }
--- AAExcelAddIn/AAExcelAddIn/PvtLstObjNavigator.cs
+++ AAExcelAddIn/AAExcelAddIn/PvtLstObjNavigator.cs
@@ -25,7 +25,7 @@ namespace AAExcelAddIn
             //Variables
             Excel.Application app;
             Excel.Workbook thisWorkbook;
-            string dataSoruceName = "", dataSrouceType = "", dataSoruceDesc = "";
+            string dataSoruceName = "", dataSrouceType = "", dataSoruceDesc = "", pageFields, rowFields, columnFields, dataFields, lstObjColumns;
 
             //Creating the activeworkbook object
             app = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
@@ -36,63 +36,201 @@ namespace AAExcelAddIn
             foreach (Excel.Worksheet ws in thisWorkbook.Worksheets)
             {
 
-                //Loading the PivotTables tab data grid
-                foreach (Excel.PivotTable pvt in ws.PivotTables())
-                {
+                //Don't load objects if they reside on a very hidden worksheet
+                if (ws.Visible != Excel.XlSheetVisibility.xlSheetVeryHidden)
+                { 
 
-                    //Determining where the data for the pivot table is being pulled from
-                    Excel.PivotCache pvtCache = pvt.PivotCache();   
-                    switch (pvt.PivotCache().SourceType)
+                    //Loading the PivotTables tab data grid
+                    foreach (Excel.PivotTable pvt in ws.PivotTables())
                     {
 
-                        //Excel Table/Range
-                        case Excel.XlPivotTableSourceType.xlDatabase:
-                            dataSoruceName = pvtCache.SourceData;
-                            dataSrouceType = "Excel Table/Range";
-                            dataSoruceDesc = "";
-                            break;
+                        //Determining where the data for the pivot table is being pulled from
+                        Excel.PivotCache pvtCache = pvt.PivotCache();
+                        switch (pvt.PivotCache().SourceType)
+                        {
 
-                        //Workbook Connection
-                        case Excel.XlPivotTableSourceType.xlExternal:
-                            dataSoruceName = pvtCache.WorkbookConnection.Name;
-                            dataSrouceType = "Workbook Connection";
-                            dataSoruceDesc = pvtCache.WorkbookConnection.Description;
-                            break;
+                            //Excel Table/Range
+                            case Excel.XlPivotTableSourceType.xlDatabase:
+                                dataSoruceName = pvtCache.SourceData;
+                                dataSrouceType = "Excel Table/Range";
+                                dataSoruceDesc = "";
+                                break;
+
+                            //Workbook Connection
+                            case Excel.XlPivotTableSourceType.xlExternal:
+                                dataSoruceName = pvtCache.WorkbookConnection.Name;
+                                dataSrouceType = "Workbook Connection";
+                                dataSoruceDesc = pvtCache.WorkbookConnection.Description;
+                                break;
+                        }
+
+                        //Creating concatenations of all the fields in the pivot
+                        //-----------------------------------------------------------------------------------
+
+                        //Filter fields
+                        pageFields = "";
+                        foreach (Excel.PivotField pvtField in pvt.PageFields)
+                        {
+                            pageFields += (pageFields == "") ? pvtField.Name : "; " + pvtField.Name;
+                        }
+
+                        //Row fields
+                        rowFields = "";
+                        foreach (Excel.PivotField pvtField in pvt.RowFields)
+                        {
+                            rowFields += (rowFields == "") ? pvtField.Name : "; " + pvtField.Name;
+                        }
+
+                        //Column fields
+                        columnFields = "";
+                        foreach (Excel.PivotField pvtField in pvt.ColumnFields)
+                        {
+                            columnFields += (columnFields == "") ? pvtField.Name : "; " + pvtField.Name;
+                        }
+
+                        //Value fields
+                        dataFields = "";
+                        foreach (Excel.PivotField pvtField in pvt.DataFields)
+                        {
+                            dataFields += (dataFields == "") ? pvtField.Name : "; " + pvtField.Name;
+                        }
+                        //-----------------------------------------------------------------------------------
+
+                        //Creating a new row in the data grid for each pivot
+                        DataGridViewRow row = new DataGridViewRow();
+                        row.CreateCells(dgrPivotTables, pvt.Name, ws.Name, dataSoruceName, dataSrouceType, dataSoruceDesc, pageFields, rowFields, columnFields, dataFields);
+                        dgrPivotTables.Rows.Add(row);
                     }
 
-                    //Creating a new row in the data grid for each pivot
-                    DataGridViewRow row = new DataGridViewRow();
-                    row.CreateCells(dgrPivotTables, pvt.Name, dataSoruceName, dataSrouceType, dataSoruceDesc);
-                    dgrPivotTables.Rows.Add(row);
+                    //Loading the List Objects tab data grid
+                    foreach (Excel.ListObject lst in ws.ListObjects)
+                    {
+
+                        //Determining where the data for the pivot table is being pulled from
+                        switch (lst.SourceType)
+                        {
+
+                            //Excel Table/Range
+                            case Excel.XlListObjectSourceType.xlSrcRange:
+                                dataSoruceName = "";
+                                dataSrouceType = "No External Source";
+                                dataSoruceDesc = "";
+                                break;
+
+                            //Workbook Connection
+                            case Excel.XlListObjectSourceType.xlSrcQuery:
+                                dataSoruceName = lst.QueryTable.WorkbookConnection.Name;
+                                dataSrouceType = "Workbook Connection";
+                                dataSoruceDesc = lst.QueryTable.WorkbookConnection.Description;
+                                break;
+                        }
+
+                        //Create a concatenation for each column in the list object
+                        lstObjColumns = "";
+                        foreach (Excel.ListColumn lstColumn in lst.ListColumns)
+                        {
+                            lstObjColumns += (lstObjColumns == "") ? lstColumn.Name : "; " + lstColumn.Name;
+                        }
+
+                        //Creating a new row in the data grid for each list object
+                        DataGridViewRow row = new DataGridViewRow();
+                        row.CreateCells(dgrListObjects, lst.Name, ws.Name, dataSoruceName, dataSrouceType, dataSoruceDesc, lstObjColumns);
+                        dgrListObjects.Rows.Add(row);
+                    }
                 }
 
-                //Loading the List Objects tab data grid
-                foreach (Excel.ListObject lst in ws.ListObjects)
+                //Unselecting the first row in the data grids
+                dgrPivotTables.ClearSelection();
+                dgrListObjects.ClearSelection();
+            }
+        }
+
+        //User double clicks to go to selected pivot in the workbook
+        private void dgrPivotTables_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+            //Variables
+            Excel.Application app;
+            Excel.Workbook thisWorkbook;
+
+            //Making sure the double clicked row isn't the header
+            if (e.RowIndex != -1)
+            {
+
+                //Creating the activeworkbook object
+                app = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
+                app.Visible = true;
+                thisWorkbook = (Excel.Workbook)app.ActiveWorkbook;
+
+                //Determining where the pivottable is
+                Excel.Worksheet ws = thisWorkbook.Sheets[dgrPivotTables.Rows[e.RowIndex].Cells[1].Value];
+                Excel.PivotTable pvt = ws.PivotTables(dgrPivotTables.Rows[e.RowIndex].Cells[0].Value);
+                Excel.Range rng = ws.Range[pvt.TableRange2.Address.Substring(0, pvt.TableRange2.Address.IndexOf(':'))];
+
+                //Checking if the worksheet the pivot resides in is hidden
+                if (ws.Visible == Excel.XlSheetVisibility.xlSheetHidden)
                 {
 
-                    //Determining where the data for the pivot table is being pulled from
-                    switch (lst.SourceType)
+                    //Confirming with the user that they wish to unhide the sheet the pivot resides on
+                    DialogResult msgboxResult = MessageBox.Show("The worksheet this PivotTable resides in is currently hidden. Do you wish the worksheet?", this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    
+                    //Unhiding the worksheet if the user confirmed it
+                    if (msgboxResult == DialogResult.Yes)
                     {
-
-                        //Excel Table/Range
-                        case Excel.XlListObjectSourceType.xlSrcRange:
-                            dataSoruceName = "";
-                            dataSrouceType = "No External Source";
-                            dataSoruceDesc = "";
-                            break;
-
-                        //Workbook Connection
-                        case Excel.XlListObjectSourceType.xlSrcQuery:
-                            dataSoruceName = lst.QueryTable.WorkbookConnection.Name;
-                            dataSrouceType = "Workbook Connection";
-                            dataSoruceDesc = lst.QueryTable.WorkbookConnection.Description;
-                            break;
+                        ws.Visible = Excel.XlSheetVisibility.xlSheetVisible;
                     }
+                }
 
-                    //Creating a new row in the data grid for each list object
-                    DataGridViewRow row = new DataGridViewRow();
-                    row.CreateCells(dgrListObjects, lst.Name, dataSoruceName, dataSrouceType, dataSoruceDesc);
-                    dgrListObjects.Rows.Add(row);
+                //Moving the cell selector to the pivot
+                if (ws.Visible == Excel.XlSheetVisibility.xlSheetVisible)
+                {
+                    ws.Select();
+                    rng.Select();
+                }
+            }
+        }
+
+        //User double clicks to go to selected list object in the workbook
+        private void dgrListObjects_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            //Variables
+            Excel.Application app;
+            Excel.Workbook thisWorkbook;
+
+            //Making sure the double clicked row isn't the header
+            if (e.RowIndex != -1)
+            {
+
+                //Creating the activeworkbook object
+                app = (Excel.Application)Marshal.GetActiveObject("Excel.Application");
+                app.Visible = true;
+                thisWorkbook = (Excel.Workbook)app.ActiveWorkbook;
+
+                //Determining where the list object is
+                Excel.Worksheet ws = thisWorkbook.Sheets[dgrListObjects.Rows[e.RowIndex].Cells[1].Value];
+                Excel.ListObject lst = ws.ListObjects[dgrListObjects.Rows[e.RowIndex].Cells[0].Value];
+                Excel.Range rng = ws.Range[lst.Range.Address.Substring(0, lst.Range.Address.IndexOf(':'))];
+
+                //Checking if the worksheet the list object resides in is hidden
+                if (ws.Visible == Excel.XlSheetVisibility.xlSheetHidden)
+                {
+
+                    //Confirming with the user that they wish to unhide the sheet the list object resides on
+                    DialogResult msgboxResult = MessageBox.Show("The worksheet this Table resides in is currently hidden. Do you wish the worksheet?", this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    //Unhiding the worksheet if the user confirmed it
+                    if (msgboxResult == DialogResult.Yes)
+                    {
+                        ws.Visible = Excel.XlSheetVisibility.xlSheetVisible;
+                    }
+                }
+
+                //Moving the cell selector to the list object
+                if (ws.Visible == Excel.XlSheetVisibility.xlSheetVisible)
+                {
+                    ws.Select();
+                    rng.Select();
                 }
             }
         }
